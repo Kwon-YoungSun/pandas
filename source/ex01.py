fa9e0e320f41177af6d5b97ab5ee6fdb7e9a839c
Kwon-YoungSun <dolphini0727@naver.com>
2021-07-27 10:17:07 +0900

    day01 2nd commit

---
+++ source/ex01.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 27 10:02:33 2021
+
+@author: Admin
+"""
+
+# pandas 불러오기
+import pandas as pd
+
+# key:value 쌍으로 딕셔너리를 만들고, 변수 dict_data에 저장
+dict_data = {'a':1, 'b':2, 'c':3}
+
+# 판다스 Series() 함수로 dictionary를 Series로 변환. 변수 sr에 저장
+sr = pd.Series(dict_data)
+
+# ar의 자료형 출력
+print(type(sr))
+print('\n')
+
+# 변수 ar에 저장되어 있는 시리즈 객체를 출력
+print(sr)
